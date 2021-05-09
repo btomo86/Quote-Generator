879b38461a630ee5b6006e2bdc1d33cd65997f74
Ben Thompson <btomo86@gmail.com>
2021-05-09 15:49:22 +0100

    can now get a new quote by clicking new quote button and also able to tweet the quote

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
 let apiQuotes = [];
 
 const newQuote = () => {
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  console.log(quote);
+  if (!quote.author) {
+    authorText.textContent = "unkown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+  if (quote.text.length > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  quoteText.textContent = quote.text;
 };
 
 async function getQuotes() {
@@ -16,4 +32,11 @@ async function getQuotes() {
   }
 }
 
+const tweetQuote = () => {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
+};
+
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
 getQuotes();
